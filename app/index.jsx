7577bf3223f4b22257526bcc9935db383552a5cb
Vinícius Varjão <moraesvxp@gmail.com>
2024-05-24 10:48:44 -0300

    style index

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Redirect } from "expo-router";
+import { Text, View } from "react-native";
 
-const index = () => <Redirect href="/(tabs)/home" />;
+const index = () => {
+  return (
+    <View className="bg-primary flex-1 items-center justify-center h-screen">
+      <Text className="text-accent text-4xl font-bold">E-hub</Text>
+      <Redirect href="/(tabs)/home" />
+    </View>
+  );
+};
 
 export default index;
