68be31a6e2d8f093b77843f01f52664982fec585
Vinícius Varjão <moraesvxp@gmail.com>
2024-05-24 10:13:52 -0300

    configure eslint and prettier

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,6 +1,5 @@
-import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
 import React from "react";
-import NewsCard from "../../components/NewsCard";
 import HomeNews from "../../components/HomeNews";
 
 const home = () => {
