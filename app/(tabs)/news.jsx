bb14c50bf1a47834104d6c12e6d9bf2c0195798f
Danniel Melo <danniel_scmelo@hotmail.com>
2024-05-27 10:51:14 -0300

    News tabs updated

--- app/(tabs)/news.jsx
+++ app/(tabs)/news.jsx
@@ -1,12 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import HomeNews from "../../components/HomeNews";
 
 const news = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-primary">
-      <Text className="text-white">news</Text>
-    </View>
+    <SafeAreaView className="bg-primary"> 
+      <ScrollView>
+        <HomeNews newsList={placeholderNews}/>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const placeholderNews = [
+  {
+    title: "New League of Legends Update Released",
+    autor: "Game Developer",
+    hour: "2021-08-15T10:00:00Z",
+  },
+  {
+    title: "The Most Popular Game in the World is Fortnite",
+    autor: "Game Analyst",
+    hour: "2021-08-10T14:30:00Z",
+  },
+  {
+    title: "New Call of Duty Game Coming Soon",
+    autor: "Gaming Website",
+    hour: "2021-08-12T09:00:00Z",
+  },
+  {
+    title: "ESL Pro League Season 10 Champion Announced",
+    autor: "ESL Announcer",
+    hour: "2021-08-15T15:30:00Z",
+  },
+];
 
 export default news;
